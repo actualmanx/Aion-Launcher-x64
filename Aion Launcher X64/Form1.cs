bdbefea096a10d13753c0f5937c749a38ed068d2
actualmanx <dilligaf1979@gmail.com>
2014-08-24 16:44:52 +0100

    First Upload
    this is the first upload of Aion 64 bit launcher.

---
+++ Aion Launcher X64/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace Aion_Launcher_X64
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+;
+
+            // Use ProcessStartInfo class
+            string argu = @"-ip:79.110.83.80 -port:2106 -noweb -noauthgg -st -charnamemenu -ingamebrowser -webshopevent:2 -f2p -lbox /SessKey:"""" /CompanyID:""11"" /ChannelGroupIndex:""-1""  -lang:ENG";
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "C:/Program Files (x86)/GameforgeLive/Games/GBR_eng/AION/Download/bin64/aion.bin";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            // No Aion Shop........ startInfo.Arguments = "-ip:79.110.83.80 -port:2106 -cc:2 -noauthgg -ls -charnamemenu -lang:eng -lbox -f2p";
+            startInfo.Arguments = argu;
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    Environment.Exit(0);
+                }
+            }
+            catch
+            {
+                // Log error.
+            }
+        }
+    }
+}
---
+++ Aion Launcher X64/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aion Launcher X64")]
+[assembly: AssemblyDescription("Launches Aion in 64 Bit")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ACID")]
+[assembly: AssemblyProduct("Aion Launcher X64")]
+[assembly: AssemblyCopyright("Copyright © ACID 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aab3d463-31f7-43dd-8a29-94e9b078c428")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
